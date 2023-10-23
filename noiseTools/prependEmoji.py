b6abfd75d022d25db01e6a0af0f01fd9826561d8
Sourcery AI <>
2023-10-23 23:26:14 +0000

    'Refactored by Sourcery'

--- noiseTools/prependEmoji.py
+++ noiseTools/prependEmoji.py
@@ -37,18 +37,14 @@ else:
 		arrayCipher = file.read().splitlines()
 
 	if ( len(sys.argv) == 1):
-	# Generate sample of noise generator output
-		i = 0
-		while ( i<20 ):
+		for _ in range(20):
 			print( random.choice(arrayCipher) + "\n" ),
-			i = i+1
-
 	else:
 	# Prepend noise generator output to file
 		with open ( sys.argv[1] ) as file:
 			cloakedFile = file.readlines()
-	
+
 		with open ( sys.argv[1], "w" ) as file:
 			for i in cloakedFile:
-				file.write( random.choice(arrayCipher) + "  " + i ),
+				(file.write(f"{random.choice(arrayCipher)}  {i}"), )
 
